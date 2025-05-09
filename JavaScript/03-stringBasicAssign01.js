0c6f9e4d12c9270358b954f464f625b9f4b48576
RajkumarGapat <rajkumargapat345@gmail.com>
2025-05-09 22:01:26 +0530

    Create 03-String Basic Assign01
    created string basic assignment file

---
+++ JavaScript/03-stringBasicAssign01.js
@@ -0,0 +1,16 @@
+function stringBasics() {
+
+    console.log(`My dream company is "J.P. Morgan"`);
+  
+    var hobby1 = "Video Games";
+    var hobby2 = "Cooking";
+    var hobby3 = "Swimming";
+  
+    console.log(`My Hobbies are: ${hobby1}, ${hobby2}, ${hobby3}`);
+  
+    let totalLength = hobby1.length + hobby2.length + hobby3.length;
+    console.log(`Total number of characters in hobbies: ${totalLength}`);
+  }
+  
+  stringBasics();
+  

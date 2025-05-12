77392bd01a8d5fbe7c4fc35d9a0d5162ff90d61a
RajkumarGapat <rajkumargapat345@gmail.com>
2025-05-12 17:11:23 +0530

    Operator & OperatorAssignment
    Created two files 05

---
+++ JavaScript/05-operatorA.js
@@ -0,0 +1,26 @@
+function squareOfWordLength(word) {
+  var strwords = word.split(" ");
+  var wordlength = strwords.length;
+  var squareOfWords = wordlength * wordlength;
+
+  console.log(`The given word is: "${word}", its length is: ${wordlength}, and its square is: ${squareOfWords}`);
+}
+
+function f3() {
+    var str = "I am MERN Stack Developer";
+    var strLength = str.length;
+    var words = str.split(" ");
+    var totalWords = words.length;
+
+    var divResult = strLength / totalWords;
+
+    var multiResult = strLength * totalWords;
+
+    console.log(`Given string is: "${str}", Length of the string is: ${strLength} & Total number of words in string is: ${totalWords}`);
+    console.log(`Result of string length divided by word count: ${divResult}`);
+    console.log(`Result of string length multiplied by word count: ${multiResult}`);
+  }
+squareOfWordLength("JavaScript");
+squareOfWordLength("Google Chrome");
+squareOfWordLength("Web Developer Smart");
+f3();
---
+++ JavaScript/05-operators.js
@@ -0,0 +1,66 @@
+console.log("***** Arithmetic Operators *****");
+
+var a = 10;
+var b = 5;
+result = a + b;
+console.log(`Addition ${result}`);   //15
+
+var result = a - b;
+console.log(`Subtraction : ${result}`);  //5
+
+var result = a / b;
+console.log(`Division: ${result}`);  //2
+
+var result = a % b;
+console.log(`Modulus: ${result}`);   //0
+
+
+console.log("");
+console.log("***** Compasrision Operators *****");
+var a = 10;
+var b = 11;
+var c = "10";
+var d = 10;
+console.log(`== Operator of a & b: ${a==b}`); //false
+console.log(`== Operator of a & d: ${a==d}`); //true
+console.log(`== Operator of a & c: ${a==c}`);  //true
+
+console.log("");
+console.log("***** (===) Operator *****");
+console.log(`=== Operator of a & b: ${a===b}`); //false
+console.log(`=== Operator of a & d: ${a===d}`); //true
+console.log(`=== Operator of a & c: ${a===c}`); //false
+
+
+console.log("");
+console.log("*****!= Not Equal Operator *****  ");
+var a = 10;
+var b = 11;
+var c = "10";
+var d = 10;
+console.log(`!= Not equal operator of a & b: ${a!=b}`);  // true
+console.log(`!= Not equal operator of a & d: ${a!=d}`);  // false
+console.log(`!= Not equal operator of a & c: ${a!=c}`);  // false
+
+console.log("");
+console.log("***** Less than (<) Operator *****  ");
+console.log(`< Less than operator between a & b: ${a<b}`);  //true
+console.log(`< Less than operator between a & d: ${a<d}`);  //false
+
+console.log("");
+console.log("***** Less than or equal (<=) Operator *****  ");
+console.log(`<= Less than equal operator between a & b: ${a<=b}`);  //true
+console.log(`<= Less than equal operator between a & d: ${a<=d}`);  //true
+
+console.log("");
+console.log("***** Greater than (>) Operator *****  ");
+console.log(`> Greater than operator between a & b: ${a>b}`);  //false
+console.log(`> Greater than operator between a & d: ${a>d}`);  //false
+
+console.log("");
+console.log("***** Greater than or equal (<=) Operator *****  ");
+console.log(`>= Greater than equal operator between a & b: ${a<=b}`); // true
+console.log(`>= Greater than equal operator between a & d: ${a<=d}`); //true 
+
+
+
